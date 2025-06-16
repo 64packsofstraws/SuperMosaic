a17ad4e984ad9c0c0e3325434e4ea609760cc975
64packsofstraws <strawaaron8@gmail.com>
2025-06-16 18:06:15 +0300

    Remove json.hpp and the ifndef DEBUG in main.cpp

--- SuperMosaic/main.cpp
+++ SuperMosaic/main.cpp
@@ -3,13 +3,8 @@
 
 int main(int argc, char** argv)
 {
-#ifndef DEBUG
 	SNES snes;
-	snes.load_file("C:\\Users\\Aaron Straw\\Downloads\\SNES-master\\PPU\\BGMAP\\8x8\\8BPP\\32x32\\8x8BGMap8BPP32x32.sfc");
+	snes.load_file("C:\\Users\\Aaron Straw\\Downloads\\SNES-master\\PPU\\BGMAP\\8x8\\8BPP\\32x64\\8x8BGMap8BPP32x64.sfc");
 	snes.run();
-#else
-	CPU cpu(nullptr);
-	cpu.step();
-#endif
 	return 0;
 }
