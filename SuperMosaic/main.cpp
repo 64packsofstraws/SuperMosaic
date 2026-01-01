dcfe5eee165010a35586073635293ae5281c1cf3
64packsofstraws <strawaaron8@gmail.com>
2026-01-01 22:38:38 +0300

    Load ROM from the path specified in the command line args if there is one

--- SuperMosaic/main.cpp
+++ SuperMosaic/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char** argv)
 {
 	SNES snes(false);
-	snes.load_file("C:\\Users\\Aaron Straw\\Downloads\\snesroms\\smw.smc");
+
+	if (argc > 1) snes.load_file(argv[1]);
+	else snes.load_file("C:\\Users\\Aaron Straw\\Downloads\\snesroms\\smw.smc");
+
 	snes.run();
 	return 0;
  }
